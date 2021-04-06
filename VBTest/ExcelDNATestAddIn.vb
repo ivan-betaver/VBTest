b14d22b19b022c439820f287f4bbd7a84cab79c9
Иван Панченко <iv.panchenko@severstal.com>
2021-04-06 21:57:23 +0300

    Add project files.

---
+++ VBTest/ExcelDNATestAddIn.vb
@@ -0,0 +1,22 @@
+﻿Imports System.Windows.Forms
+Imports ExcelDna.Integration
+Imports ExcelDna.IntelliSense
+
+Public Class ExcelDNATestAddIn
+    Implements IExcelAddIn
+
+    Public Sub AutoOpen() Implements IExcelAddIn.AutoOpen
+        IntelliSenseServer.Install()
+    End Sub
+
+    Public Sub AutoClose() Implements IExcelAddIn.AutoClose
+        IntelliSenseServer.Uninstall()
+    End Sub
+
+    <ExcelFunction("VB.NET test function")>
+    Public Shared Function addSum(<ExcelArgument("Введите значение")> input As Object) As Object
+        MessageBox.Show(input.ToString)
+        Return input
+    End Function
+
+End Class
